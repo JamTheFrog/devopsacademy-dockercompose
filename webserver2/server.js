f73b0bdc788734f66beac72fce247345d0b4457f
Faris Srnja <faris.srnja2004@gmail.com>
2025-01-27 17:08:05 +0100

    valjda radi

---
+++ webserver2/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const redis = require("redis");
+const path = require("path");
+
+const app = express();
+const PORT = 9200;
+
+const client = redis.createClient({
+  url: 'redis://redis:6379'
+});
+
+client.on('error', (err) => {
+  console.error('Redis error:', err);
+});
+
+const retryRedisConnection = async () => {
+  try {
+    await client.connect();
+    console.log('Connected to Redis');
+  } catch (err) {
+    console.error('Redis connection failed. Retrying...', err);
+    setTimeout(retryRedisConnection, 5000);
+  }
+};
+
+retryRedisConnection();
+
+app.use(async (req, res, next) => {
+  try {
+    const webserver2Count = await client.incr('webserver2_count');
+    const totalVisits = await client.incr('total_visits');
+    
+    res.locals.webserver2Count = webserver2Count;
+    res.locals.totalVisits = totalVisits;
+    next();
+  } catch (err) {
+    console.error("Error incrementing Redis keys:", err);
+    next(err);
+  }
+});
+
+
+app.use('/static', express.static(path.join(__dirname, "public")));
+
+
+app.get("/", (req, res) => {
+  const greeting = `Webserver 2 is running on http://localhost:${PORT}`;
+  res.send(`
+    <h1>${greeting}</h1>
+    <p>You have visited webserver2: ${res.locals.webserver2Count} times.</p>
+    <p>You have visited both webservers: ${res.locals.totalVisits} times.</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Webserver 2 is running on http://localhost:${PORT}`);
+});
